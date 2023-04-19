7193a86ec396847a2f2182b1c7822ecca9c072a7
Fujipp <anwker3008@gmail.com>
2023-04-19 13:11:11 +0700

    a

---
+++ int103_087/src/int103_087/Int103_087.java
@@ -0,0 +1,10 @@
+package int103_087;
+
+
+public class Int103_087 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ int103_087/src/int103_087/Shape.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package int103_087;
+
+/**
+ *
+ * @author a.
+ */
+public interface Shape {
+    public void draw();
+}
+
